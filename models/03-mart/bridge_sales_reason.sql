ed6163681ed2bdac83cdddb16bea2bd3054a37e5
Carlos Felippe <cfrazevedo@gmail.com>
2026-02-02 19:45:24 +0000

    Ajustes na bridge

--- models/03-mart/bridge_sales_reason.sql
+++ models/03-mart/bridge_sales_reason.sql
@@ -3,7 +3,7 @@ with
     int_salesreason as (
 
         select
-            sales_order_fk
+            sales_order_id
             , sales_reason_sk
         from {{ ref('int_salesreason') }}
 
@@ -11,7 +11,9 @@ with
 
     , int_sales as (
 
-        select sales_sk
+        select 
+            sales_sk
+            , sales_order_id
         from {{ ref('int_sales') }}
 
     )
@@ -20,11 +22,11 @@ with
 
         select
             int_sales.sales_sk as sales_fk
-            , int_salesreason.sales_order_fk
+            , int_salesreason.sales_order_id
             , int_salesreason.sales_reason_sk as sales_reason_fk
         from int_sales
         full join int_salesreason
-        on int_sales.sales_order_fk = int_salesreason.sales_order_fk
+        on int_sales.sales_order_id = int_salesreason.sales_order_id
         where int_salesreason.sales_reason_sk is not null
 
     )
@@ -32,7 +34,7 @@ with
     , final as (
 
         select
-            {{ dbt_utils.generate_surrogate_key(['sales_order_fk', 'sales_reason_fk', 'sales_fk']) }} as bridge_sales_reason_sk
+            {{ dbt_utils.generate_surrogate_key(['sales_reason_fk', 'sales_fk']) }} as bridge_sales_reason_sk
             , sales_fk
             , sales_reason_fk
             , current_timestamp() as updated_at
